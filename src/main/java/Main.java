18dcf6c289a0c49f72b6833d0f8abc856e8a28fc
Art <art.off.work@gmail.com>
2020-06-09 00:32:16 +0300

    downgrade java to 11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,20 @@ public class Main {
             var menu = new Menu(args[0]);
             var app = new App();
             switch (menu.getCommand()) {
-                case "volumes" -> app.printVolumesList();
-                case "find" -> app.findAndPrintFiles(args[1], args[2]);
-                case "read" -> app.readFile(args[1]);
-                case "write" -> app.writeFile(args[1], args[2]);
-                default -> System.out.println("Fin");
+                case "volumes":
+                    app.printVolumesList();
+                    break;
+                case "find":
+                    app.findAndPrintFiles(args[1], args[2]);
+                    break;
+                case "read":
+                    app.readFile(args[1]);
+                    break;
+                case "write":
+                    app.writeFile(args[1], args[2]);
+                    break;
+                default:
+                    System.out.println("Fin");
             }
         }
     }
